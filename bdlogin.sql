926624dbdcd4054009b61a0f7fc2e7ffa0c00b07
esthercosta <67116620+esthercosta@users.noreply.github.com>
2021-09-08 21:41:34 -0300

    envio do banco de dados

---
+++ bdlogin.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 07-Set-2021 às 09:40
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bdlogin`
+--
+CREATE DATABASE IF NOT EXISTS `bdlogin` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bdlogin`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tblogin`
+--
+
+CREATE TABLE `tblogin` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tblogin`
+--
+
+INSERT INTO `tblogin` (`id`, `nome`, `email`, `senha`) VALUES
+(0, 'Esther', 'oliveiracostaesther@gmail.com', '1234');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
